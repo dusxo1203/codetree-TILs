13f9b56098b031d43a5ea3e7b7a80f393b36044e
yyt <dusxo1203@gmail.com>
2024-02-05 00:12:29 +0900

    Title: 특정 위치의 문자 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240205/특정 위치의 문자 2/char-in-specific-location-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+char s[12];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    for(int i=0;i<10;i++)
+    {
+        cin>>s[i];
+    }
+    cout<<s[1]<<' '<<s[4]<<' '<<s[7];
+
+    return 0;
+}
