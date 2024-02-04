9c638afe0b65320fa0bb2e4d3f641c822844fa5d
yyt <dusxo1203@gmail.com>
2024-02-05 01:35:42 +0900

    Title: 전항의 두 배, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240205/전항의 두 배/twice-the-previous.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int a,b;
+int arr[12];
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin>>a>>b;
+    arr[0] = a;
+    arr[1] = b;
+    for(int i=2;i<10;i++)
+    {
+        arr[i] = arr[i-1] + 2*arr[i-2];
+
+    }
+    for(int i=0;i<10;i++)
+    {
+        cout<<arr[i]<<' ';
+
+    }
+
+    return 0;
+}
